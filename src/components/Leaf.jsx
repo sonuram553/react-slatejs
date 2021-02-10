892d37a08f6b54ef8d8a8767a99d180fcc1fc2b4
Sonu Ram <sonuram6960@gmail.com>
2021-02-11 01:46:00 +0530

    'ctrl + b' makes selected text bold

---
+++ src/components/Leaf.jsx
@@ -0,0 +1,10 @@
+export const Leaf = (props) => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
+    >
+      {props.children}
+    </span>
+  );
+};
--- src/components/SlateEditor.jsx
+++ src/components/SlateEditor.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable default-case */
 import React, { useState, useMemo } from "react";
-import { createEditor, Transforms, Editor } from "slate";
+import { createEditor, Transforms, Editor, Text } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 import { withHistory } from "slate-history";
 
 import { CodeElement } from "./CodeELement";
 import { DefaultElement } from "./DefaultElement";
+import { Leaf } from "./Leaf";
 
 const initialValue = [
   {
@@ -32,26 +34,47 @@ export const SlateEditor = () => {
     }
   };
 
+  const onKeyDown = (e) => {
+    if (!e.ctrlKey) return null;
+
+    switch (e.key) {
+      case "`": {
+        e.preventDefault();
+        const [match] = Editor.nodes(editor, {
+          match: (n) => n.type === "code",
+        });
+
+        Transforms.setNodes(
+          editor,
+          { type: match ? "paragraph" : "code" },
+          { match: (n) => Editor.isBlock(editor, n) }
+        );
+
+        break;
+      }
+
+      case "b": {
+        e.preventDefault();
+        Transforms.setNodes(
+          editor,
+          { bold: true },
+          { match: (n) => Text.isText(n), split: true }
+        );
+
+        break;
+      }
+    }
+  };
+
+  const renderLeaf = (props) => <Leaf {...props} />;
+
   return (
     <Slate editor={editor} value={value} onChange={setValue}>
       <Editable
         placeholder="Enter some plain text ..."
         renderElement={renderElement}
-        onKeyDown={(e) => {
-          if (e.key === "`" && e.ctrlKey) {
-            e.preventDefault();
-
-            const [match] = Editor.nodes(editor, {
-              match: (n) => n.type === "code",
-            });
-
-            Transforms.setNodes(
-              editor,
-              { type: match ? "paragraph" : "code" },
-              { match: (n) => Editor.isBlock(editor, n) }
-            );
-          }
-        }}
+        renderLeaf={renderLeaf}
+        onKeyDown={onKeyDown}
       />
     </Slate>
   );
